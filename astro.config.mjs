575cb9c19912f8fd762bb81033a5fd999726ccd2
jonaseriksson84 <jonaseriksson84@gmail.com>
2024-03-24 23:29:36 +0100

    auth ok

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import db from "@astrojs/db";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), db()],
+  vite: {
+    optimizeDeps: {
+      exclude: ["oslo", "astro:db"]
+    }
+  },
+  output: 'server',
+});
---
+++ db/config.ts
@@ -0,0 +1,33 @@
+// db/config.ts
+import { column, defineDb, defineTable } from "astro:db";
+
+const User = defineTable({
+	columns: {
+		id: column.text({
+			primaryKey: true
+		}),
+    username: column.text({
+      unique: true,
+    }),
+    hashed_password: column.text(),
+	}
+});
+
+const Session = defineTable({
+	columns: {
+		id: column.text({
+			primaryKey: true
+		}),
+		expiresAt: column.date(),
+		userId: column.text({
+			references: () => User.columns.id
+		})
+	}
+});
+
+export default defineDb({
+	tables: {
+		User,
+		Session
+	}
+});
